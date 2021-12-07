5e2b8d2fb6ef146c2dc6847917e6ce48f6b56166
DoniaMahmoud <donia1999mahmoud@gmail.com>
2021-12-07 17:37:50 +0200

    Final Project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//Redux Setup
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
+
+//Redux Thunk allows us to do async fetching of data in our action creator
+// We cannot fetch data using axios in aciton creators , instead use Redux Thunk
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//CreateStore accepts 2 args only
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/reducers/detailReducer.js
@@ -0,0 +1,31 @@
+const initState = {
+  game: {
+    platforms: [],
+  },
+  screen: {
+    results: [],
+  },
+  //Wait for data to be loaded then dislay
+  isLoading: true,
+};
+
+const detailReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "GET_DETAIL":
+      return {
+        ...state,
+        game: action.payload.game,
+        screen: action.payload.screen,
+        isLoading: false,
+      };
+    case "LOADING_DETAIL":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export default detailReducer;
